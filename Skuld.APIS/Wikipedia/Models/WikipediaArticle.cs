74a078810cd36c6f075cd2e5195d89ef4cce556b
exsersewo <exsersewo@systemexit.co.uk>
2020-03-31 21:39:08 +0100

    Tweak Wiki Client

--- Skuld.APIS/Wikipedia/Models/WikipediaArticle.cs
+++ Skuld.APIS/Wikipedia/Models/WikipediaArticle.cs
@@ -5,5 +5,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Url { get; set; }
+        public WikipediaImage Original { get; set; }
     }
 }
--- Skuld.APIS/Wikipedia/WikipediaClient.cs
+++ Skuld.APIS/Wikipedia/WikipediaClient.cs
@@ -2,6 +2,7 @@
 using Skuld.APIS.Wikipedia.Models;
 using Skuld.Core.Utilities;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Skuld.APIS
@@ -19,15 +20,49 @@ namespace Skuld.APIS
         {
             if (rateLimiter.IsRatelimited()) return null;
 
-            var jsonresp = JObject.Parse(await HttpWebClient.ReturnStringAsync(new Uri($"https://{language}.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles={name}")).ConfigureAwait(false));
-            dynamic item = jsonresp["query"]["pages"].First.First;
-            string desc = Convert.ToString(item["extract"]);
             var article = new WikipediaArticle
             {
-                Name = item["title"].ToString(),
-                Description = desc.Remove(500) + "...\nRead more at the article.",
                 Url = $"https://{language}.wikipedia.org/wiki/{name}"
             };
+
+            {
+                var pageInfo = await HttpWebClient.ReturnStringAsync(new Uri($"https://{language}.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles={name}")).ConfigureAwait(false);
+
+                var jsonresp = JObject.Parse(pageInfo);
+                dynamic item = jsonresp["query"]["pages"].First.First;
+
+                string desc = Convert.ToString(item["extract"]);
+
+                article.Name = item["title"].ToString();
+
+                if (desc.Length >= 1024)
+                {
+                    var split = desc.Split(". ").ToList();
+                    article.Description = string.Join(". ", split.Take(4))+".";
+                }
+                else
+                {
+                    article.Description = desc;
+                }
+            }
+
+            {
+                var pageImage = await HttpWebClient.ReturnStringAsync(new Uri($"https://{language}.wikipedia.org/w/api.php?format=json&action=query&prop=pageimages&piprop=original&titles={name}")).ConfigureAwait(false);
+
+                var jsonresp = JObject.Parse(pageImage);
+                dynamic item = jsonresp["query"]["pages"].First.First["original"];
+
+                if(item != null)
+                {
+                    article.Original = new WikipediaImage
+                    {
+                        Source = item["source"].ToString(),
+                        Width = Convert.ToInt32(item["width"]),
+                        Height = Convert.ToInt32(item["height"])
+                    };
+                }
+            }
+
             return article;
         }
     }
